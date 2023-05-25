9b33186042f454b4ecfa92c4b9f08909ca5cc797
yasen002 <362yasen2@gmail.com>
2023-05-24 21:11:26 -0700

    UYG10 - create language slice

--- src/app/types.ts
+++ src/app/types.ts
@@ -3,3 +3,64 @@ import { ConnectedComponent } from "react-redux";
 export type InjectComp =
   | ConnectedComponent<(prop: any) => JSX.Element, any>
   | ((prop?: any) => JSX.Element);
+
+export type Languages = {
+  english: string;
+  turkish: string;
+  uyghur: string;
+  chinese: string;
+};
+export type Language = {
+  home: {
+    header: {
+      headerNav1: Languages;
+      headerNav2: Languages;
+    };
+    right: {
+      rHeader3: Languages;
+      rSubHeader4: Languages;
+      text: {
+        rNav5: Languages;
+        rCardHolder8: Languages;
+        rCardTitle9: Languages;
+        rDropLabel10: Languages;
+        rDropLabel11: Languages;
+      };
+      font: {
+        rNav6: Languages;
+        rCardTitle16: Languages;
+        rDropLabel17: Languages;
+        rDropLabel18: Languages;
+        rDropLabel19: Languages;
+        rDropLabel20: Languages;
+        rCardTitle21: Languages;
+      };
+
+      page: {
+        rNav7: Languages;
+        rCardTitle22: Languages;
+        rDropLabel23: Languages;
+        rDropLabel24: Languages;
+        rCardTitle25: Languages;
+        rCardOption26: Languages;
+        rCardOption27: Languages;
+        rCardTitle28: Languages;
+        rCardOption29: Languages;
+        rCardOption30: Languages;
+        rCardOption31: Languages;
+        rCardOption32: Languages;
+      };
+      rBtn12: Languages;
+      rBtn13: Languages;
+      rBtn14: Languages;
+    };
+    left: {
+      lHeader15: Languages;
+    };
+    footer: {
+      fTitle33: Languages;
+      fSubTitle34: Languages;
+      fPhone: Languages;
+    };
+  };
+};
---
+++ src/features/language/initialState.ts
@@ -0,0 +1,231 @@
+import { Language } from "../../app/types";
+
+const dummyData: Language = {
+  home: {
+    header: {
+      headerNav1: {
+        english: "English",
+        turkish: "Turkish",
+        uyghur: "Uyghur",
+        chinese: "Chinese",
+      },
+      headerNav2: {
+        english: "English",
+        turkish: "Turkish",
+        uyghur: "Uyghur",
+        chinese: "Chinese",
+      },
+    },
+    right: {
+      rHeader3: {
+        english: "English",
+        turkish: "Turkish",
+        uyghur: "Uyghur",
+        chinese: "Chinese",
+      },
+      rSubHeader4: {
+        english: "English",
+        turkish: "Turkish",
+        uyghur: "Uyghur",
+        chinese: "Chinese",
+      },
+      text: {
+        rNav5: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rCardHolder8: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rCardTitle9: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rDropLabel10: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rDropLabel11: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+      },
+      font: {
+        rNav6: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rCardTitle16: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rDropLabel17: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rDropLabel18: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rDropLabel19: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rDropLabel20: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rCardTitle21: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+      },
+
+      page: {
+        rNav7: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rCardTitle22: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rDropLabel23: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rDropLabel24: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rCardTitle25: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rCardOption26: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rCardOption27: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rCardTitle28: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rCardOption29: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rCardOption30: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rCardOption31: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+        rCardOption32: {
+          english: "English",
+          turkish: "Turkish",
+          uyghur: "Uyghur",
+          chinese: "Chinese",
+        },
+      },
+      rBtn12: {
+        english: "English",
+        turkish: "Turkish",
+        uyghur: "Uyghur",
+        chinese: "Chinese",
+      },
+      rBtn13: {
+        english: "English",
+        turkish: "Turkish",
+        uyghur: "Uyghur",
+        chinese: "Chinese",
+      },
+      rBtn14: {
+        english: "English",
+        turkish: "Turkish",
+        uyghur: "Uyghur",
+        chinese: "Chinese",
+      },
+    },
+    left: {
+      lHeader15: {
+        english: "English",
+        turkish: "Turkish",
+        uyghur: "Uyghur",
+        chinese: "Chinese",
+      },
+    },
+    footer: {
+      fTitle33: {
+        english: "English",
+        turkish: "Turkish",
+        uyghur: "Uyghur",
+        chinese: "Chinese",
+      },
+      fSubTitle34: {
+        english: "English",
+        turkish: "Turkish",
+        uyghur: "Uyghur",
+        chinese: "Chinese",
+      },
+      fPhone: {
+        english: "English",
+        turkish: "Turkish",
+        uyghur: "Uyghur",
+        chinese: "Chinese",
+      },
+    },
+  },
+};
