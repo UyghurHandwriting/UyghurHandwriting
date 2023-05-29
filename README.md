2c97f625983cbf2d9b0eb0c41dbddbc0d90e9d47
yasen002 <362yasen2@gmail.com>
2023-05-29 14:10:59 -0700

    UYG - 21 configure SCSS, CSS

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Uyghur Handwriting Tracing tool
+
+### Get Stared
+`yarn` to install packages
+`yarn start` to start the app
--- webpack.config.js
+++ webpack.config.js
@@ -41,8 +41,17 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        test: /\.(s[ac]ss|css)$/i,
+        // test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
       {
         test: /\.svg$/,
