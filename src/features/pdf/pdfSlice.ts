78b193f047dab1b9b58a2dcfe9965ca57666d807
yasen002 <362yasen2@gmail.com>
2023-06-13 20:39:38 -0700

    3-uyg-14 baseline&font alignment

--- src/features/pdf/pdfSlice.ts
+++ src/features/pdf/pdfSlice.ts
@@ -3,7 +3,6 @@ import { RootState } from "../../app/store";
 import { LanguageKeys, RowSize } from "../../app/types";
 
 export type PdfInitialState = {
-  fontSize: number;
   language: LanguageKeys;
   baseLineSize: RowSize;
   baseLineColor: string;
@@ -12,30 +11,21 @@ export type PdfInitialState = {
 };
 
 export const initialState: PdfInitialState = {
-  fontSize: 55,
   language: "uyghur",
-  baseLineSize: "LG",
+  baseLineSize: "XL",
   baseLineColor: "rgba(103,102,102,255)",
-  baseLineGap: 20,
-  text: "  خ خ خ خ خ خ خ ختى   ئىلگىرى خ خ خ خ خ خ خ خ ختى ئىلگىرى رەھبەرلىك باسق خ خ خ خ خ خ خ خ خ خ خ خ خ خ خ تۈركىيەنىڭ يېڭى ھۆكۈمەت كابىنېتىنىڭ بۇنىڭدىن كېيىنكى ئىچكى سىياسىتى، بولۇپمۇ ئىقتىسادىي سىياسىتى تۈركىيەلىكلەر ئەڭ كۆڭۈل بۆلىدىغان مەسىلە، ئەمما بۇ قېتىمقى سايلامغا ناھايىتى كۆڭۈل بۆلگەن ئۇيغۇرلارنىڭ كۆڭۈل بۆلىدىغىنى يېڭى كابىنېتنىڭ خىتاي ۋە ئۇيغۇرلار مەسىلىسىگە قانداق مۇئامىلە تۇتۇشىدۇر. ئەلۋەتتە، ئۇيغۇرلار تۈركىيەنىڭمۇ خۇددى ئامېرىكا ۋە باشقا غەرب دېموكراتىك دۆلەتلىرىدەك خىتاينىڭ ئۇيغۇرلارغا يۈرگۈزۈۋاتقانلىرىنى ئىرقىي قىرغىنچىلىق ۋە ئىنسانىيەتكە قارشى جىنايەت دەپ بېكىتىشىنى ئۈمىد قىلىدۇ. لېكىن تۈركىيە ھۆكۈمىتى ئىلگىرى رەھبەرلىك باسقۇچىدا بۇنداق قەدەم تاشلىمىدى. خ خ خ خ خ خ خ خ خ ختى ئىلگىرى رەھبەرلى ك باسق",
+  baseLineGap: 10,
+  text: "  ئابپتجچخدرزژسشغفقكڭگلمنھەوۆۇۈۋيىې ئ ا ب پ ت ج چ خ د ر ز ژ س ش غ ف ق ك ڭ گ ل م ن ھ ە و ۆ ۇ ۈ ۋ ي ى ې  ئابپتجچخدرزژسشغفقكڭگلمنھەوۆۇۈۋيىې ئ ا ب پ ت ج چ خ د ر ز ژ س ش غ ف ق ك ڭ گ ل م ن ھ ە و ۆ ۇ ۈ ۋ ي ى ې  ئابپتجچخدرزژسشغفقكڭگلمنھەوۆۇۈۋيىې ئ ا ب پ ت ج چ خ د ر ز ژ س ش غ ف ق ك ڭ گ ل م ن ھ ە و ۆ ۇ ۈ ۋ ي ى ې  ئابپتجچخدرزژسشغفقكڭگلمنھەوۆۇۈۋيىې ئ ا ب پ ت ج چ خ د ر ز ژ س ش غ ف ق ك ڭ گ ل م ن ھ ە و ۆ ۇ ۈ ۋ ي ى ې  ئابپتجچخدرزژسشغفقكڭگلمنھەوۆۇۈۋيىې ئ ا ب پ ت ج چ خ د ر ز ژ س ش غ ف ق ك ڭ گ ل م ن ھ ە و ۆ ۇ ۈ ۋ ي ى ې ئابپتجچخدرزژسشغفقكڭگلمنھەوۆۇۈۋيىې ئ ا ب پ ت ج چ خ د ر ز ژ س ش غ ف ق ك ڭ گ ل م ن ھ ە و ۆ ۇ ۈ ۋ ي ى ې ئابپتجچخدرزژسشغفقكڭگلمنھەوۆۇۈۋيىې ئ ا ب پ ت ج چ خ د ر ز ژ س ش غ ف ق ك ڭ گ ل م ن ھ ە و ۆ ۇ ۈ ۋ ي ى ې ئابپتجچخدرزژسشغفقكڭگلمنھەوۆۇۈۋيىې ئ ا ب پ ت ج چ خ د ر ز ژ س ش غ ف ق ك ڭ گ ل م ن ھ ە و ۆ ۇ ۈ ۋ ي ى ې ئابپتجچخدرزژسشغفقكڭگلمنھەوۆۇۈۋيىې ئ ا ب پ ت ج چ خ د ر ز ژ س ش غ ف ق ك ڭ گ ل م ن ھ ە و ۆ ۇ ۈ ۋ ي ى ې",
 };
 
 export const sliceKey = "pdf";
 export const pdfSlice = createSlice({
   name: sliceKey,
   initialState,
-  reducers: {
-    setPdfFontSize: (
-      state,
-      action: PayloadAction<PdfInitialState["fontSize"]>
-    ) => {
-      state.fontSize = action.payload;
-    },
-  },
+  reducers: {},
 });
 
 //export reducers & selector
-export const { setPdfFontSize } = pdfSlice.actions;
+export const {} = pdfSlice.actions;
 export const pdfReducer = pdfSlice.reducer;
-export const selectPdfFontSize = (state: RootState) => state[sliceKey].fontSize;
 export const selectPdfSlice = (state: RootState) => state[sliceKey];
