4b0cd059942bf6d96dd8850011b716c39c30078a
yasen002 <362yasen2@gmail.com>
2023-06-25 13:37:53 -0700

    uyg-9 image dropdown component

---
+++ src/component/ImageDropdown/ImageDropdown.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { InjectComp } from "../../app/types";
+import { LanguageIcon } from "../ImageBox/ImageBox";
+import { LanguageOption } from "../Dropdown/Dropdown";
+import { connect } from "react-redux";
+import { RootState } from "../../app/store";
+import { selectLangOptionOpen } from "../../features/language/languageSlice";
+import { EmptyComp } from "../EmptyComp";
+
+type HandleOptionClick = (prop: any) => void;
+interface ComponentProps {
+  DropdownImg: InjectComp;
+  DropdownOption: InjectComp;
+}
+interface ComponentDispatch {
+  handleOptionClick?: HandleOptionClick;
+}
+type Props = ComponentProps & ComponentDispatch;
+
+export const ImageDropdown = ({ DropdownImg, DropdownOption }: Props) => {
+  return (
+    <div>
+      <DropdownImg />
+      <DropdownOption />
+    </div>
+  );
+};
+
+export default ImageDropdown;
+
+export const LangImgDropdown = connect(
+  //-----mapProps
+  (state: RootState, ownProps: {}): ComponentProps => {
+    const langOptionOpen = selectLangOptionOpen(state);
+    console.log(langOptionOpen);
+    return {
+      DropdownImg: LanguageIcon,
+      DropdownOption: langOptionOpen ? LanguageOption : EmptyComp,
+    };
+  }
+)(ImageDropdown);
